241b172553b023f5cb4077396aaa565f7d08cd57
DiaecicalWayfarer <2208161439@qq.com>
2025-12-12 15:46:22 +0800

    2025.12.12

---
+++ Test2/regex_test.py
@@ -0,0 +1,20 @@
+# import re
+# pattern=r"mr_\w+"
+# string="MR_SHOP mr_shop"
+# match=re.match(pattern,string,re.I)
+# print(match)
+# string="项目名称MR_SHOP mr_shop"
+# match=re.match(pattern,string,re.I)
+# print(match)
+
+# import re
+# pattern=r"mr_\w+"
+# string="MR_SHOP mr_shop"
+# match=re.match(pattern,string,re.I)
+# print("起始位置：",match.start())
+# print("结束位置：",match.end())
+# print("元组：",match.span())
+# print("匹配字符串",match.string)
+# print("匹配数据",match.group())
+
+  
